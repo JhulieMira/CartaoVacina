0573647dd012006f2252927d9eaab4ce3c5a9713
Jhulie Mira <jhuliemira@gmail.com>
2025-06-28 18:36:40 -0300

    fix: added missing mappings for user entity

--- CartaoVacina.Core/Mappers/UserProfile.cs
+++ CartaoVacina.Core/Mappers/UserProfile.cs
@@ -9,5 +9,10 @@ public class UserProfile: Profile
     public UserProfile()
     {
         CreateMap<User, UserDTO>();
+        CreateMap<CreateUserDTO, User>()
+            .ForMember(
+                dest => dest.Gender, 
+                opt => opt.MapFrom(src => string.IsNullOrEmpty(src.Gender) ? Gender.Other : Enum.Parse<Gender>(src.Gender, true))
+            );
     }
 }
